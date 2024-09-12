d7cc1f62db7f2454194525fe6cd67c9064bb8f11
Vân Anh <89188722+yw07761@users.noreply.github.com>
2024-09-12 13:31:47 +0700

    Cập nhật giao diện
    Giao diện đăng ký, đăng nhập, quên mật khẩu, lấy mã code, đặt lại mật khẩu

--- src/app/forgot-password/forgot-password.component.ts
+++ src/app/forgot-password/forgot-password.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.component.html',
-  //styleUrls: ['./forgot-password.component.css']
+  styleUrls: ['./forgot-password.component.scss']
 })
 export class ForgotPasswordComponent {
   email: string = '';
