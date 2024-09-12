78436ce0513fededa04254f67cb6b7af6f006898
Vân Anh <89188722+yw07761@users.noreply.github.com>
2024-09-12 13:25:48 +0700

    Update login, sign up, forgot pw
    Cập nhật giao diện đăng nhập, đăng ký, quên mật khẩu, lấy mã xác minh, đặt lại mật khẩu

---
+++ qrcode/qrcode.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-qrcode',
+  standalone: true,
+  imports: [],
+  templateUrl: './qrcode.component.html',
+  styleUrl: './qrcode.component.scss'
+})
+export class QRcodeComponent {
+
+}
---
+++ reset-pw/reset-pw.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reset-pw',
+  standalone: true,
+  imports: [],
+  templateUrl: './reset-pw.component.html',
+  styleUrl: './reset-pw.component.scss'
+})
+export class ResetPWComponent {
+
+}
