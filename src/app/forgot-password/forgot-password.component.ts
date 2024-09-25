3d7b2af13ff13e0f8b7165a26185868b82e6116d
Vân Anh <89188722+yw07761@users.noreply.github.com>
2024-09-25 12:17:24 +0700

    route
    thêm route-link cho các page

--- src/app/forgot-password/forgot-password.component.ts
+++ src/app/forgot-password/forgot-password.component.ts
@@ -1,7 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-forgot-password',
+  standalone: true,
+  template: `
+    <a routerLink="/login">login</a>
+  `, 
+  imports: [RouterLink, RouterOutlet],
   templateUrl: './forgot-password.component.html',
   styleUrls: ['./forgot-password.component.scss']
 })
