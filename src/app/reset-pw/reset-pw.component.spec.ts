b6bff71dc5a8bdcb5f0c82a261439b3feea3dd75
Vân Anh <89188722+yw07761@users.noreply.github.com>
2024-09-25 15:21:54 +0700

    Change bootstrap
    Using bootstrap for login page, register page, forgot-pw page and reset password

--- src/app/reset-pw/reset-pw.component.spec.ts
+++ src/app/reset-pw/reset-pw.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ResetPWComponent } from './reset-pw.component';
+import { beforeEach, describe, it } from 'node:test';
 
 describe('ResetPWComponent', () => {
   let component: ResetPWComponent;
