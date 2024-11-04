62183abbc07c8b4e4fdc4a123332caae0236e43b
VanAnh <vananh7702@gmail.com>
2024-11-04 18:42:51 +0700

    Bonus Profile and Setting Account

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
-  standalone: true,
-  imports: [],
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.scss'
+  styleUrls: ['./profile.component.scss'],
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule
+  ]
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  user: any = null;
 
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {
+    this.user = this.authService.getCurrentUser (); // Lấy thông tin người dùng
+  }
 }
